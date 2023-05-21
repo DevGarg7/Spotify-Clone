fdecd8682b36ee948a7e7b2e718ac1347a5fb0d2
DevGarg7 <123263694+DevGarg7@users.noreply.github.com>
2023-05-21 23:01:38 +0530

    commit

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ let gif = document.getElementById('gif');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Salam-e-Ishq1",filePath:"songs/1.mp3",coverPath: "covers/1.jpg"},
-    {songName: "Salam-e-Ishq2",filePath:"songs/2.mp3",coverPath: "covers/2.jpg"},
+    {songName: "Ye Tune Kya Kia",filePath:"songs/1.mp3",coverPath: "covers/1.jpg"},
+    {songName: "Mere Rashke Qamar",filePath:"songs/2.mp3",coverPath: "covers/2.jpg"},
     {songName: "Salam-e-Ishq3",filePath:"songs/3.mp3",coverPath: "covers/3.jpg"},
     {songName: "Salam-e-Ishq4",filePath:"songs/4.mp3",coverPath: "covers/4.jpg"},
     {songName: "Salam-e-Ishq5",filePath:"songs/5.mp3",coverPath: "covers/5.jpg"},
@@ -29,14 +29,14 @@ songItems.forEach((element , i)=>{
 masterPlay.addEventListener('click',()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
-        masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
         gif.style.opacity=1;
     }
     else{
         audioElement.pause();
-        masterPlay.classList.remove('fa-pause-circle');
-        masterPlay.classList.add('fa-play-circle');
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play');
         gif.style.opacity=0;
     }
 })
@@ -55,8 +55,8 @@ myProgressBar.addEventListener('change' , ()=> {
 
 const makeAllPlays = () => {
     Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
-    element.classList.add('fa-pause-circle');
-    element.classList.add('fa-play-circle');
+    element.classList.remove('fa-circle-pause');
+    element.classList.add('fa-circle-play');
 
     })
 }
@@ -65,7 +65,13 @@ const makeAllPlays = () => {
 Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
     element.addEventListener('click' , (e)=>{
         makeAllPlays();
-        e.target.classList.remove('fa-play-circle');
-        e.target.classList.add('fa-pause-circle');
+        index = parseInt(e.target.id);
+        e.target.classList.remove('fa-circle-play');
+        e.target.classList.add('fa-circle-pause');
+        audioElement.src = `songs/${index+1}.mp3`;
+        audioElement.currentTime=0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
     })
 })
